a1993c56144388484b674e3cccfd8b2a1647b43e
Adviento <630766@bah.com>
2023-07-05 15:34:50 -0700

    Added account controller, login authentication with JWT token, and cleaned up program.cs file with extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,15 +5,14 @@
 
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // GET /api/users
-
-    public class UsersController : ControllerBase
+    [Authorize]
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -21,6 +20,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
