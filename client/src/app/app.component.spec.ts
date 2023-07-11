516a0e014fed19005e4ce922bcdd7e26a8303b33
Adviento <630766@bah.com>
2023-07-11 14:25:42 -0700

    Added angular routes, auth guards, and styling from Bootswatch themes

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        HttpClientTestingModule,
+        RouterTestingModule,
+        RouterOutlet
       ],
       declarations: [
         AppComponent
+        
       ],
     }).compileComponents();
   });
@@ -20,16 +25,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'Dating App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('Dating App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
+    expect(compiled.querySelector('router-outlet')).not.toBe(null);
   });
 });
